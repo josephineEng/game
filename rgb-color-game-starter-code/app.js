b8a35c1b461bb4b63387c3288507e81414e200dd
JosephineBonka <67620450+josephineEng@users.noreply.github.com>
2021-12-30 09:33:33 +0300

    updates

--- rgb-color-game-starter-code/app.js
+++ rgb-color-game-starter-code/app.js
@@ -23,12 +23,46 @@ levels.forEach((level) =>{
 //setting square background colors using rgb
 const startButton=document.getElementById("reset");
 startButton.addEventListener("click",function(){
+  // assign each sqaure a background color
   for(let i=0;i< squares.length;i++){
-    const firstSquare =squares[i];
-    firstSquare.style.backgroundColor="rgb(200,45,255)";
+    const red=Math.floor(Math.random()* 256); // generating random color values
+    const green=Math.floor(Math.random()* 256);
+    const blue=Math.floor(Math.random()* 256);
+
+    const rgbString="rgb("+red+","+green+","+blue+")";
+    const square =squares[i];
+    square.dataset.rgb_value =JSON.stringify([red,green,blue]);
+    square.style.backgroundColor=rgbString;
+
  
   }
- 
+  // assigning the headers rgb values of one of the squares
+  const randomSquareIndex = Math.floor(Math.random()* 6);
+  const headerColorSquare=squares[randomSquareIndex];
+  setRgbBackgroundColor(headerColorSquare);
   
-
 });
+
+function setRgbBackgroundColor(squareElement){
+  const setHeaderElementBackgroundColor = (rgbValues,element)=>{
+    const [r,g,b] =rgbValues;
+    const rgbString = `rgb(${r},${g},${b})`;
+    element.style.backgroundColor =rgbString;
+    //assigning the numbers corresponding the color of the headers
+    element.innerHTML =rgbValues.find((rgbValue)=>{
+      return rgbValue>0;
+
+    });
+  };
+  const rgbString = squareElement.dataset.rgb_value;
+  const [red,green,blue] =JSON.parse(rgbString);
+
+  const redBackground=[red,0,0];
+  const greenBackground=[0,green,0];
+  const blueBackground=[0,0,blue];
+
+  setHeaderElementBackgroundColor(redBackground,rElement);
+  setHeaderElementBackgroundColor(greenBackground,gElement);
+  setHeaderElementBackgroundColor(blueBackground,bElement);
+ 
+}
